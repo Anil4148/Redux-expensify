b6422779353e236b36781a5d02a80385f83b2c6c
anilwonmentapps <anil.kumar@wondermentapps.com>
2021-03-01 12:01:08 +0530

    Initial Commit

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,10 @@
+import moment from 'moment';
+import { setStartDate, setEndDate } from "../../actions/filters";
+
+// test("should generate set start date action object", () => {
+//   const action = setStartDate(moment(0));
+//   expect(action).toStrictEqual({
+//     type: "SET_START_DATE",
+//     setStartDate: moment(0)
+//   });
+// });
