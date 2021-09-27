becfcb25439fcd0cffa64c0f29f4999fcd3d437d
anilwonmentapps <anil.kumar@wondermentapps.com>
2021-09-27 12:08:31 +0530

    Update about and policy

--- src/components/About.js
+++ src/components/About.js
@@ -8,29 +8,8 @@ const About = () => {
             <div className="row">
               <div className="col-md-6 col-xs-12 about-sec">
                 <p>
-                  Hi there. We’re a new financial manager platform that will harness the power of financial manager to inspire,
-                  entertain, and transform lives.
-                </p>
-                <p>
-                  We’ll be sharing a host of original, unexpected, commercial-free programming ranging from comedies to
-                  dramas, realities to docu-series.
-                </p>
-                <p>
-                  Our shows will deal with relationships, career, money, family and a lot more relevant and difficult
-                  subjects in transparent, unforgettable ways. We’ll explore what it’s like to raise a family ​right
-                  now​, to build a career off the beaten path, to become an entrepreneur in today’s volatile economy,
-                  and to make clear the unique challenges Millennials and Gen Z are facing. Sounds like a lot, right?
-                  Well, adulting is​ a lot.
-                </p>
-                <p>
-                  Through these stories, some inspiring, some difficult, and some laugh out loud funny, we hope to take
-                  the taboo out of certain subjects and transform them into a comfortable conversation that’s both
-                  compelling as well as personally empowering.
-                </p>
-                <p>
-                  Even in today’s difficult market, you can be stronger financially, and one of our main goals is to
-                  help make that happen. 
-                </p>
+                 Coming Soon....
+                </p>               
               </div>
             </div>
           </div>
